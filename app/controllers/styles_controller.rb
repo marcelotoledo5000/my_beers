dd85ce9574ec2e635b6a4e9b37ad1a53ecbde398
Marcelo Toledo <marcelotoledo5000@gmail.com>
2018-11-04 20:14:56 -0200

    Adding tests, routes, helpers and StylesController

---
+++ app/controllers/styles_controller.rb
@@ -0,0 +1,42 @@
+class StylesController < ApplicationController
+  before_action :set_style, only: [:show, :update, :destroy]
+
+  # GET /styles
+  def index
+    @styles = Style.all
+    json_response(@styles)
+  end
+
+  # GET /styles/:id
+  def show
+    json_response(@style)
+  end
+
+  # POST /styles
+  def create
+    @style = Style.create!(style_params)
+    json_response(@style, :created)
+  end
+
+    # PUT /styles/:id
+    def update
+      @style.update(style_params)
+      head :no_content
+    end
+
+    # DELETE /styles/:id
+    def destroy
+      @style.destroy
+      head :no_content
+    end
+
+  private
+
+  def style_params
+    params.permit(:name, :school_brewery)
+  end
+
+  def set_style
+    @style = Style.find(params[:id])
+  end
+end
