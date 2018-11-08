0748e49ce362134636488be60ca1a3053110c7b7
Marcelo Toledo <marcelotoledo5000@gmail.com>
2018-11-08 12:44:01 -0200

    Small Refactoring

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,28 +9,44 @@ class Ability
     else
       can :read, :all
       if user.default?
-        can :create, Pub
-        can :create, Style
-        can :create, Beer
-        can :update, Pub do |pub|
-          pub.try(:user) == user
-        end
-        can :update, Style do |style|
-          style.try(:user) == user
-        end
-        can :update, Beer do |beer|
-          beer.try(:user) == user
-        end
-        can :destroy, Pub do |pub|
-          pub.try(:user) == user
-        end
-        can :destroy, Style do |style|
-          style.try(:user) == user
-        end
-        can :destroy, Beer do |beer|
-          beer.try(:user) == user
-        end
+        can_create
+
+        can_update(user)
+
+        can_destroy(user)
       end
+    end
+  end
+
+  private
+
+  def can_create
+    can :create, Pub
+    can :create, Style
+    can :create, Beer
+  end
+
+  def can_destroy(user)
+    can :destroy, Pub do |pub|
+      pub.try(:user) == user
+    end
+    can :destroy, Style do |style|
+      style.try(:user) == user
+    end
+    can :destroy, Beer do |beer|
+      beer.try(:user) == user
+    end
+  end
+
+  def can_update(user)
+    can :update, Pub do |pub|
+      pub&.user == user # pub.try(:user)
+    end
+    can :update, Style do |style|
+      style&.user == user # style.try(:user)
+    end
+    can :update, Beer do |beer|
+      beer&.user == user # beer.try(:user)
     end
   end
 end
