5523d4cebf1f60dcf4ce3ed62ec3f6754c9f22a6
Marcelo Toledo <marcelotoledo5000@gmail.com>
2018-11-11 17:43:31 -0200

    Do not store the password in plain text

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'bcrypt', '~> 3.1', '>= 3.1.12'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'cancancan', '~> 2.3'
 gem 'pg', '>= 0.18', '< 2.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :pubs, dependent: :destroy
   has_many :styles, dependent: :destroy
 
+  has_secure_password
+
   validates :email, :password, presence: true
 
   enum role: %i[guest default admin]
