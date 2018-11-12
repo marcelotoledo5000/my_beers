ce6b9ad8e03732943ff7f45fc15ff0ad5b529051
Marcelo Toledo <marcelotoledo5000@gmail.com>
2018-11-12 01:37:29 -0200

    Fix beers spec with new authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   has_secure_password
 
-  validates :email, :password, presence: true
+  validates :email, presence: true, uniqueness: true
 
   enum role: %i[guest default admin]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_11_11_184127) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role", default: 0
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "beers", "styles"
