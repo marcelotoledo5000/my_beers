2dd58e8e8ae2aaf2322c5c5888f6eb9cbca4a505
Marcelo Toledo <marcelotoledo5000@gmail.com>
2020-05-23 20:26:25 -0300

    Fix some issues from CodeClimate

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -22,33 +22,21 @@ class Ability
     can :create, Beer
   end
 
-  def can_destroy(user)
-    can :destroy, Pub do |pub|
+  def can_do_it(user, action)
+    can action, Pub do |pub|
       pub&.user == user
     end
-    can :destroy, Style do |style|
+    can action, Style do |style|
       style&.user == user
     end
-    can :destroy, Beer do |beer|
-      beer&.user == user
-    end
-  end
-
-  def can_update(user)
-    can :update, Pub do |pub|
-      pub&.user == user
-    end
-    can :update, Style do |style|
-      style&.user == user
-    end
-    can :update, Beer do |beer|
+    can action, Beer do |beer|
       beer&.user == user
     end
   end
 
   def can_execute(user)
     can_create
-    can_update(user)
-    can_destroy(user)
+    can_do_it(user, :update)
+    can_do_it(user, :destroy)
   end
 end
